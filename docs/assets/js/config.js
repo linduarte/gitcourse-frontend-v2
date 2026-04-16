614dfadef443ff57890c31c887e2f28e90487b82
Charles Duarte (Work) <140444382+linduarte@users.noreply.github.com>
2026-04-16 15:09:36 -0300

    refactor(config): restructure API_URL and REPO_BASE into CONFIG object

--- docs/assets/js/config.js
+++ docs/assets/js/config.js
@@ -1,2 +1,5 @@
 // docs/assets/js/config.js
-export const API_URL = "https://charles-gitcourse.duckdns.org";
+export const CONFIG = {
+    API_URL: "https://charles-gitcourse.duckdns.org",
+    REPO_BASE: "/gitcourse-frontend-v2/curso/git-course/"
+};
