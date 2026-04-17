f54cb74dee977d6b75000a15581e92b5113bf765
Charles Duarte (Work) <140444382+linduarte@users.noreply.github.com>
2026-04-17 13:42:16 -0300

    fix(config): update API_URL to include port 8080 for correct endpoint access

--- docs/assets/js/config.js
+++ docs/assets/js/config.js
@@ -1,4 +1,5 @@
-export const API_URL = "https://charles-gitcourse.duckdns.org";
+// Adicione o :8080 ao final do domínio
+export const API_URL = "https://charles-gitcourse.duckdns.org:8080";
 
 export const CONFIG = {
     API_URL: API_URL,
