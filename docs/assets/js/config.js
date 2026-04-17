8c8d948549a1da4a9523613f78d53d2f328f3e9e
Charles Duarte (Work) <140444382+linduarte@users.noreply.github.com>
2026-04-17 17:26:42 -0300

    fix(config): update API_URL to use HTTPS for secure endpoint access

--- docs/assets/js/config.js
+++ docs/assets/js/config.js
@@ -1,5 +1,5 @@
 // Adicione o :8080 ao final do domínio
-export const API_URL = "http://charles-gitcourse.duckdns.org:8000";
+export const API_URL = "https://charles-gitcourse.duckdns.org:8000";
 
 export const CONFIG = {
     API_URL: API_URL,
