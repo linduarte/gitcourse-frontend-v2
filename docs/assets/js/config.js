96562da08fda50a624030c0ae581953293c77156
Charles Duarte (Work) <140444382+linduarte@users.noreply.github.com>
2026-04-21 06:56:32 -0300

    feat(config): remove unnecessary port comment from API_URL definition

--- docs/assets/js/config.js
+++ docs/assets/js/config.js
@@ -1,5 +1,3 @@
-// Adicione o :8080 ao final do domínio
-// export const API_URL = "https://charles-gitcourse.duckdns.org:8000";
 export const API_URL = "https://charles-gitcourse.duckdns.org";
 
 export const CONFIG = {
